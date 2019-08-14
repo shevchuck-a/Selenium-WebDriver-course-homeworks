56047d2e38a852a05b51d295c0a33e094a251081
Alexandr Shevchuck <shevchuck.a@gmail.com>
2019-08-14 17:57:29 +0300

    try to switch browser tabs

--- Lesson 4/Task 7/Task 7/XPathLocators.cs
+++ Lesson 4/Task 7/Task 7/XPathLocators.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -20,19 +21,32 @@ namespace Task_7
         }
 
         [Test]
-        public void LoginTest()
+        public void SideMenuTest()
         {
-            driver.Url = "http://localhost/litecart/";
+            driver.Url = "http://localhost/litecart/en/";
+            driver.Manage().Cookies.DeleteAllCookies();
             driver.Manage().Cookies.AddCookie(new Cookie("LCSESSID", "7a6rrp1vf48kp4852hk9inh92h"));
-            driver.Url = "http://localhost/litecart/admin/login.php";
+            driver.Url = "http://localhost/litecart/admin/";
 
-            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.TitleIs("My Store"));
-        }
+            //driver.FindElements(By.XPath("//ul[@id='box-apps-menu']//a"));
 
-        [Test]
-        public void MenuItems()
-        {
+            ReadOnlyCollection<IWebElement> links = driver.FindElements(By.XPath("//ul[@id='box-apps-menu']//a"));
 
+            foreach (IWebElement link in links)
+            {
+                link.SendKeys(Keys.Control + Keys.Enter);
+                //driver.SwitchTo().Window(driver.WindowHandles);
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.CssSelector("h1")));
+                driver.Navigate().Back();
+            }
+
+            //driver.FindElement(By.XPath("//span[text()='Appearence']/..")).Click();
+            //wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.TextToBePresentInElement
+            //    (driver.FindElement(By.CssSelector("h1")), "Template"));
+
+            //driver.FindElement(By.XPath("//span[text()='Logotype']/..")).Click();
+            //wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.TextToBePresentInElement
+            //    (driver.FindElement(By.CssSelector("h1")), "Logotype"));
         }
 
         [TearDown]
