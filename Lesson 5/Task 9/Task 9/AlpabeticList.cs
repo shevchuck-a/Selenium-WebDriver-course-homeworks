77648c7f6017f601bf4efa96e1ebd408f573d13d
Alexandr Shevchuck <shevchuck.a@gmail.com>
2019-08-16 16:41:36 +0300

    Update AlpabeticList.cs

--- Lesson 5/Task 9/Task 9/AlpabeticList.cs
+++ Lesson 5/Task 9/Task 9/AlpabeticList.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -11,6 +12,7 @@ namespace Task_9
     {
         private IWebDriver driver;
         private WebDriverWait wait;
+        string[] countries;
 
         [SetUp]
         public void start()
@@ -20,6 +22,25 @@ namespace Task_9
         }
 
         [Test]
+        public void ListSortingTest()
+        {
+            driver.Url = "http://localhost/litecart/en/";
+            driver.Manage().Cookies.DeleteAllCookies();
+            driver.Manage().Cookies.AddCookie(new Cookie("LCSESSID", "7a6rrp1vf48kp4852hk9inh92h"));
+            driver.Url = "http://localhost/litecart/admin/?app=countries&doc=countries";
 
+            ReadOnlyCollection<IWebElement> rows = driver.FindElements(By.TagName("tr"));
+            int countCountries = rows.Count;
+
+            foreach (IWebElement row in rows)
+            {
+                string country = row.FindElement(By.XPath("//td[5]/a")).GetAttribute("text");
+                for (int i = 0; i < countCountries; i++)
+                {
+                    countries[i] = country;
+                }
+            }
+
+        }
     }
 }
