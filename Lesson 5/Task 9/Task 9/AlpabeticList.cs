a9bf2808cadbcff9d67b1ed6a5a364f3230112e1
Alexandr Shevchuck <shevchuck.a@gmail.com>
2019-08-16 16:37:54 +0300

    creating array of countries for sorting

---
+++ Lesson 5/Task 9/Task 9/AlpabeticList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Task_9
+{
+    [TestFixture]
+    class AlpabeticList
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+
+    }
+}
