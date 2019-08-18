f054359b79375522da58bff001fd8859192a299d
Alexander Shevchuck <shevchuck.a@gmail.com>
2019-08-18 23:11:11 +0300

    Update AlpabeticList.cs

--- Lesson 5/Task 9/Task 9/AlpabeticList.cs
+++ Lesson 5/Task 9/Task 9/AlpabeticList.cs
@@ -22,25 +22,48 @@ namespace Task_9
         }
 
         [Test]
-        public void ListSortingTest()
+        public void CountriesSortingTest()
         {
-            driver.Url = "http://localhost/litecart/en/";
-            driver.Manage().Cookies.DeleteAllCookies();
-            driver.Manage().Cookies.AddCookie(new Cookie("LCSESSID", "7a6rrp1vf48kp4852hk9inh92h"));
+            driver.Url = "http://localhost/litecart/admin/login.php";
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
             driver.Url = "http://localhost/litecart/admin/?app=countries&doc=countries";
 
-            ReadOnlyCollection<IWebElement> rows = driver.FindElements(By.TagName("tr"));
+            ReadOnlyCollection<IWebElement> rows = driver.FindElements(By.CssSelector(".row"));
+            //System.IO.File.WriteAllLines(@"c:\Users\Admin\Documents\GitHub\FindElements.txt", rows);
             int countCountries = rows.Count;
 
-            foreach (IWebElement row in rows)
+            for (int i = 1; i <= countCountries; i++)
             {
-                string country = row.FindElement(By.XPath("//td[5]/a")).GetAttribute("text");
-                for (int i = 0; i < countCountries; i++)
-                {
-                    countries[i] = country;
-                }
+                string country = driver.FindElement(By.XPath("//tr[@class='row'][" + i + "]/td/a")).GetAttribute("text");
+                //countries[i] = country;
             }
+            System.IO.File.WriteAllLines(@"c:\Users\Admin\Documents\GitHub\WriteLines.txt", countries);
+            //foreach (IWebElement row in rows)
+            //{
+            //    string country = row.FindElement(By.XPath("//td[5]/a")).GetAttribute("text");
 
+            //    for (int i = 0; i < countCountries; i++)
+            //    {
+
+            //        //countries[i] = country;
+            //        System.IO.File.WriteAllText(@"c:\Users\Admin\Documents\GitHub\WriteText.txt", country);
+            //    }
+            //}
+
+            //string[] sortedCountries = Array.Sort(countries);
+            //if (sortedCountries != countries)
+            //{
+            //    Console.WriteLine("Countries are not sorted");   
+            //}
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
         }
     }
 }
