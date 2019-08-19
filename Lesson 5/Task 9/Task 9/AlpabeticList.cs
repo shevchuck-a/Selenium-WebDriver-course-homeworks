39a03f9b854fc867a68e4f0524266830341ffd50
Alexandr Shevchuck <shevchuck.a@gmail.com>
2019-08-19 18:11:00 +0300

    not completed zones check

--- Lesson 5/Task 9/Task 9/AlpabeticList.cs
+++ Lesson 5/Task 9/Task 9/AlpabeticList.cs
@@ -1,9 +1,11 @@
 ﻿using System;
-using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
+using System.Collections;
 
 namespace Task_9
 {
@@ -12,7 +14,6 @@ namespace Task_9
     {
         private IWebDriver driver;
         private WebDriverWait wait;
-        string[] countries;
 
         [SetUp]
         public void start()
@@ -30,16 +31,61 @@ namespace Task_9
             driver.FindElement(By.Name("login")).Click();
             driver.Url = "http://localhost/litecart/admin/?app=countries&doc=countries";
 
-            ReadOnlyCollection<IWebElement> rows = driver.FindElements(By.CssSelector(".row"));
-            //System.IO.File.WriteAllLines(@"c:\Users\Admin\Documents\GitHub\FindElements.txt", rows);
-            int countCountries = rows.Count;
+            //Test of alphabetical order of Countries
+            ArrayList text = new ArrayList();
+            IList<IWebElement> countries = driver.FindElements(By.XPath("//tr[@class='row']/td[5]/a"));
+            int countCountries = countries.Count;
+            var unsortedCountries = new List<string>();
 
             for (int i = 1; i <= countCountries; i++)
             {
-                string country = driver.FindElement(By.XPath("//tr[@class='row'][" + i + "]/td/a")).GetAttribute("text");
-                //countries[i] = country;
+                string country = driver.FindElement(By.XPath("//tr[@class='row'][" + i + "]/td/a"))
+                    .GetAttribute("text");
+                unsortedCountries.Add(country);
             }
-            System.IO.File.WriteAllLines(@"c:\Users\Admin\Documents\GitHub\WriteLines.txt", countries);
+
+            var sortedCountries = new List<string>();
+            sortedCountries.AddRange(unsortedCountries.OrderBy(o => o));
+            Assert.IsTrue(unsortedCountries.SequenceEqual(sortedCountries));
+
+            //Test of alphabetical order of Countrie's zones
+            for (int i = 1; i <= countCountries; i++)
+            {
+                string enabledZones = driver.FindElement(By.XPath("//tr[@class='row'][" + i + "]/td[6]"))
+                    .GetAttribute("textContent");
+                int countZones = Convert.ToInt32(enabledZones);
+                var unsortedZones = new List<string>();
+                if (countZones != 0)
+                {
+                    driver.FindElement(By.XPath("//tr[@class='row'][" + i + "]/td[5]/a")).Click();
+                    //IList<IWebElement> zones = driver.FindElements(By.XPath("//table[@id='table-zones']//td[3]/input"));
+                    //int countZones = zones.Count;
+                    for (int j = 2; j <= ++countZones; j++)
+                    {
+                        string zone = driver.FindElement
+                            (By.XPath("//table[@id='table-zones']//tr[" + i + "]/td[3]/input")).GetAttribute("value");
+                        unsortedZones.Add(zone);
+                    }
+                    var sortedZones = new List<string>();
+                    sortedZones.AddRange(unsortedZones.OrderBy(o => o));
+                    Assert.IsTrue(unsortedZones.SequenceEqual(sortedZones));
+
+                }
+            }
+
+
+            //ReadOnlyCollection<IWebElement> rows = driver.FindElements(By.CssSelector(".row"));
+            ////System.IO.File.WriteAllLines(@"c:\Users\Admin\Documents\GitHub\FindElements.txt", rows);
+            //int countCountries = rows.Count;
+
+            //for (int i = 1; i <= countCountries; i++)
+            //{
+            //    string country = driver.FindElement(By.XPath("//tr[@class='row'][" + i + "]/td/a")).GetAttribute("text");
+            //    //countries[i] = country;
+            //}
+            //System.IO.File.WriteAllLines(@"c:\Users\Admin\Documents\GitHub\WriteLines.txt", countries);
+
+
             //foreach (IWebElement row in rows)
             //{
             //    string country = row.FindElement(By.XPath("//td[5]/a")).GetAttribute("text");
