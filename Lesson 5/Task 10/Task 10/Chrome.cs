7890a8b6510aa37b497386f1bdc071698e9f3a47
Alexander Shevchuck <shevchuck.a@gmail.com>
2019-08-20 23:45:00 +0300

    not edited string with font size

---
+++ Lesson 5/Task 10/Task 10/Chrome.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Task_10
+{
+    class Chrome
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void Start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void ProductTest()
+        {
+            driver.Url = "http://localhost/litecart/en/";
+
+            //values for comparing with values from product page
+            string title = driver.FindElement(By.CssSelector("#box-campaigns .name")).GetAttribute("textContent");
+            string price = driver.FindElement(By.CssSelector("#box-campaigns .campaign-price")).GetAttribute("textContent");
+            string priceStyle = driver.FindElement(By.CssSelector("#box-campaigns .campaign-price")).TagName;
+            string regularPrice = driver.FindElement(By.CssSelector("#box-campaigns .regular-price")).GetAttribute("textContent");
+            string regularPriceStyle = driver.FindElement(By.CssSelector("#box-campaigns .regular-price")).TagName;
+
+            //check that Price color is red (G and B from RGB are equal 0)
+            string priceColourData = driver.FindElement(By.CssSelector("#box-campaigns .campaign-price")).GetCssValue("color");
+            String[] hexPriceColour = priceColourData.Replace("rgba(", "").Replace(")", "").Split(',');
+            int hexPriceColour2 = Convert.ToInt32(hexPriceColour[1]);
+            int hexPriceColour3 = Convert.ToInt32(hexPriceColour[2]);
+            Assert.AreEqual(0, hexPriceColour2);
+            Assert.AreEqual(0, hexPriceColour3);
+
+            //check that Regular Price is gray (RGB values are equal)
+            string regularPriceColourData = driver.FindElement(By.CssSelector("#box-campaigns .regular-price")).GetCssValue("color");
+            String[] hexRegularPriceColour = regularPriceColourData.Replace("rgba(", "").Replace(")", "").Split(',');
+            int hexRegularPriceColour1 = Convert.ToInt32(hexRegularPriceColour[0]);
+            int hexRegularPriceColour2 = Convert.ToInt32(hexRegularPriceColour[1]);
+            int hexRegularPriceColour3 = Convert.ToInt32(hexRegularPriceColour[2]);
+            Assert.AreEqual(hexRegularPriceColour1, hexRegularPriceColour2);
+            Assert.AreEqual(hexRegularPriceColour1, hexRegularPriceColour3);
+
+            //check that font size of Regular Prise is less than 1
+            string regularPriseData = driver.FindElement(By.CssSelector("#box-campaigns .regular-price")).GetCssValue("font-size");
+            regularPriseData.Replace("px", "");
+            Convert.ToInt32(regularPriseData);
+            string priseData = driver.FindElement(By.CssSelector("#box-campaigns .campaign-price")).GetCssValue("font-size");
+            Convert.ToInt32(priseData.Replace("px", ""));
+            Assert.Greater(priseData, regularPriseData);
+
+            //
+            driver.FindElement(By.CssSelector("#box-campaigns .link")).Click();
+
+            string productTitle = driver.FindElement(By.CssSelector("h1")).GetAttribute("textContent");
+            Assert.AreEqual(title, productTitle);
+
+            string productPrice = driver.FindElement(By.CssSelector(".campaign-price")).GetAttribute("textContent");
+            Assert.AreEqual(price, productPrice);
+
+            string productPriceStyle = driver.FindElement(By.CssSelector(".campaign-price")).TagName;
+            Assert.AreEqual(priceStyle, productPriceStyle);
+
+            string productRegularPrice = driver.FindElement(By.CssSelector(".regular-price")).GetAttribute("textContent");
+            Assert.AreEqual(regularPrice, productRegularPrice);
+
+            string productRegularPriceStyle = driver.FindElement(By.CssSelector(".regular-price")).TagName;
+            Assert.AreEqual(regularPriceStyle, productRegularPriceStyle);
+
+            //check that Price color on Product page is red (G and B from RGB are equal 0)
+            string productPriceColourData = driver.FindElement(By.CssSelector(".campaign-price")).GetCssValue("color");
+            String[] hexProductPriceColour = productPriceColourData.Replace("rgba(", "").Replace(")", "").Split(',');
+            int hexProductPriceColour2 = Convert.ToInt32(hexProductPriceColour[1]);
+            int hexProductPriceColour3 = Convert.ToInt32(hexProductPriceColour[2]);
+            Assert.AreEqual(0, hexProductPriceColour2);
+            Assert.AreEqual(0, hexProductPriceColour3);
+
+            //check that Regular Price on Product page is gray (RGB values are equal)
+            string productRegularPriceColourData = driver.FindElement(By.CssSelector(".regular-price")).GetCssValue("color");
+            String[] hexProductRegularPriceColour = productRegularPriceColourData.Replace("rgba(", "").Replace(")", "").Split(',');
+            int hexProductRegularPriceColour1 = Convert.ToInt32(hexProductRegularPriceColour[0]);
+            int hexProductRegularPriceColour2 = Convert.ToInt32(hexProductRegularPriceColour[1]);
+            int hexProductRegularPriceColour3 = Convert.ToInt32(hexProductRegularPriceColour[2]);
+            Assert.AreEqual(hexProductRegularPriceColour1, hexProductRegularPriceColour2);
+            Assert.AreEqual(hexProductRegularPriceColour1, hexProductRegularPriceColour3);
+
+            //check that font size of Regular Prise on Product page is less than 1
+            string productRegularPriseData = driver.FindElement(By.CssSelector(".regular-price")).GetCssValue("font-size");
+            Convert.ToInt32(productRegularPriseData.Replace("px", ""));
+            string productPriseData = driver.FindElement(By.CssSelector(".campaign-price")).GetCssValue("font-size");
+            Convert.ToInt32(productPriseData.Replace("px", ""));
+            Assert.Greater(productPriseData, productRegularPriseData);
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
---
+++ Lesson 5/Task 10/Task 10/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Task 10")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Task 10")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a73cea4a-9b33-4d2f-aae6-7f1e2fa8f641")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
