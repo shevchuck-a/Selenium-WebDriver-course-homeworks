f1a4d2b283517277694f215ce226c3173e1bb4ce
Alexandr Shevchuck <shevchuck.a@gmail.com>
2019-08-27 17:32:54 +0300

    task 13 is done

--- Selenium-WebDriver-course-homeworks/Lesson 7/Task 13/WorkingWithCart.cs
+++ Selenium-WebDriver-course-homeworks/Lesson 7/Task 13/WorkingWithCart.cs
@@ -22,13 +22,48 @@ namespace Selenium_WebDriver_course_homeworks.Lesson_7.Task_13
         [Test]
         public void BuyDucks()
         {
-            driver.Url = "http://localhost/litecart/en/";
-            driver.FindElement(By.ClassName("product")).Click();
+            driver.Url = "https://litecart.stqa.ru/en/";
+            for (int i = 1; i < 4; i++)
+            {
+                driver.FindElement(By.ClassName("product")).Click();
 
-            driver.FindElement(By.CssSelector("[name = add_cart_product]")).Click();
-            IAlert alert = driver.SwitchTo().Alert();
-            alert.Accept();
+                
+                if (IsElementPresent(By.CssSelector("[name=\"options[Size]\"]")))
+                {
+                    driver.FindElement(By.CssSelector("[name=\"options[Size]\"]")).Click();
+                    driver.FindElement(By.CssSelector("[name=\"options[Size]\"] option:nth-child(2)")).Click();
+                }
+                driver.FindElement(By.CssSelector("[name = add_cart_product]")).Click();
+                IWebElement cart = driver.FindElement(By.CssSelector("#cart .content .quantity"));
+                string count = Convert.ToString(i);
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                wait.Until(ExpectedConditions.TextToBePresentInElement(cart, count));
 
+                driver.Navigate().Back();
+                Console.WriteLine(i);
+            }
+            driver.FindElement(By.CssSelector("#cart .link")).Click();
+
+            if (IsElementPresent(By.CssSelector("[name=remove_cart_item]")))
+            {
+                IWebElement table = driver.FindElement(By.Id("box-checkout-summary"));
+                driver.FindElement(By.CssSelector("[name=remove_cart_item]")).Click();
+                wait.Until(ExpectedConditions.StalenessOf(table));
+            }
+
+        }
+
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
         }
 
         [TearDown]
