12156d15d3405407e58f2fcb518fa0c8d691e533
Alexander Shevchuck <shevchuck.a@gmail.com>
2019-09-05 22:54:47 +0300

    Revert "browserstack example"
    This reverts commit ba2475d3aaff354dd2e5ea475eabe33736e646eb.

--- Selenium-WebDriver-course-homeworks/Lesson 9/Task 16/CloudGrid.cs
+++ Selenium-WebDriver-course-homeworks/Lesson 9/Task 16/CloudGrid.cs
@@ -1,38 +1,85 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Linq;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
 
 namespace Selenium_WebDriver_course_homeworks.Lesson_9.Task_16
 {
     [TestFixture]
     public class BrowserStackNUnitTest
     {
-        [Test]
-        public void Browserstack()
+        protected IWebDriver driver;
+        protected string profile;
+        protected string environment;
+        private BrowserStack.Local browserStackLocal;
+
+        public BrowserStackNUnitTest(string profile, string environment)
         {
-            IWebDriver driver;
-            ChromeOptions options = new ChromeOptions();
-            options.AddAdditionalCapability("browser", "Chrome");
-            options.AddAdditionalCapability("browser_version", "62.0");
-            options.AddAdditionalCapability("os", "Windows");
-            options.AddAdditionalCapability("os_version", "10");
-            options.AddAdditionalCapability("resolution", "1024x768");
-            options.AddAdditionalCapability("browserstack.user", "-");
-            options.AddAdditionalCapability("browserstack.key", "-");
-            options.AddAdditionalCapability("name", "Bstack-[C_sharp] Sample Test");
+            this.profile = profile;
+            this.environment = environment;
+        }
 
-            driver = new RemoteWebDriver(new Uri("http://hub-cloud.browserstack.com/wd/hub/"), options);
-            driver.Navigate().GoToUrl("http://www.google.com");
-            Console.WriteLine(driver.Title);
+        [SetUp]
+        public void Init()
+        {
+            NameValueCollection caps = ConfigurationManager.GetSection("capabilities/" + profile) as NameValueCollection;
+            NameValueCollection settings = ConfigurationManager.GetSection("environments/" + environment) as NameValueCollection;
 
-            IWebElement query = driver.FindElement(By.Name("q"));
-            query.SendKeys("Browserstack");
-            query.Submit();
-            Console.WriteLine(driver.Title);
+            DesiredCapabilities capability = new DesiredCapabilities();
 
+            foreach (string key in caps.AllKeys)
+            {
+                capability.SetCapability(key, caps[key]);
+            }
+
+            foreach (string key in settings.AllKeys)
+            {
+                capability.SetCapability(key, settings[key]);
+            }
+
+            String username = Environment.GetEnvironmentVariable("BROWSERSTACK_USERNAME");
+            if (username == null)
+            {
+                username = ConfigurationManager.AppSettings.Get("user");
+            }
+
+            String accesskey = Environment.GetEnvironmentVariable("BROWSERSTACK_ACCESS_KEY");
+            if (accesskey == null)
+            {
+                accesskey = ConfigurationManager.AppSettings.Get("key");
+            }
+
+            capability.SetCapability("browserstack.user", username);
+            capability.SetCapability("browserstack.key", accesskey);
+            capability.SetCapability("name", "Bstack-[Nunit] Sample Test");
+
+            if (capability.GetCapability("browserstack.local") != null && capability.GetCapability("browserstack.local").ToString() == "true")
+            {
+                browserStackLocal = new BrowserStack.Local();
+                List<KeyValuePair<string, string>> bsLocalArgs = new List<KeyValuePair<string, string>>() {
+        new KeyValuePair<string, string>("key", accesskey)
+      };
+                browserStackLocal.start(bsLocalArgs);
+            }
+
+            driver = new RemoteWebDriver(new Uri("http://" + ConfigurationManager.AppSettings.Get("server") + "/wd/hub/"), capability);
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
             driver.Quit();
+            if (browserStackLocal != null)
+            {
+                browserStackLocal.stop();
+            }
         }
     }
 }
---
+++ Selenium-WebDriver-course-homeworks/Lesson 9/Task 16/CloudGrid2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+
+namespace Selenium_WebDriver_course_homeworks.Lesson_9.Task_16
+{
+    [TestFixture]
+    class Program
+    {
+        [Test]
+        public void Browserstack()
+        {
+            //IWebDriver driver = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), options); ;
+            //ChromeOptions options = new ChromeOptions();
+            //DesiredCapabilities capability;
+            //ChromeOptions().AddAdditionalCapability("browser", "Chrome");
+            IWebDriver driver;
+
+            ChromeOptions options = new ChromeOptions();
+            options.AddAdditionalCapability("browser", "Chrome");
+            options.AddAdditionalCapability("browser_version", "62.0");
+            options.AddAdditionalCapability("os", "Windows");
+            options.AddAdditionalCapability("os_version", "10");
+            options.AddAdditionalCapability("resolution", "1024x768");
+            options.AddAdditionalCapability("browserstack.user", "bsuser52951");
+            options.AddAdditionalCapability("browserstack.key", "LP8AYuT9xtLvxTzw9TBU");
+            options.AddAdditionalCapability("name", "Bstack-[C_sharp] Sample Test");
+
+            //DesiredCapabilities capability = DesiredCapabilities.Chrome();
+            //capability.SetCapability("browser", "Chrome");
+            //capability.SetCapability("browser_version", "62.0");
+            //capability.SetCapability("os", "Windows");
+            //capability.SetCapability("os_version", "10");
+            //capability.SetCapability("resolution", "1024x768");
+            //capability.SetCapability("browserstack.user", "bsuser52951");
+            //capability.SetCapability("browserstack.key", "LP8AYuT9xtLvxTzw9TBU");
+            //capability.SetCapability("name", "Bstack-[C_sharp] Sample Test");
+
+            driver = new RemoteWebDriver(new Uri("http://hub-cloud.browserstack.com/wd/hub/"), options);
+            driver.Navigate().GoToUrl("http://www.google.com");
+            Console.WriteLine(driver.Title);
+
+            IWebElement query = driver.FindElement(By.Name("q"));
+            query.SendKeys("Browserstack");
+            query.Submit();
+            Console.WriteLine(driver.Title);
+
+            driver.Quit();
+        }
+    }
+}
